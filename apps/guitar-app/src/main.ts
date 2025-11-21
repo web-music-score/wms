af18aa85c00081d6c818e82f8c846146d44829d4
Stefan B. <web-music-score@outlook.com>
2025-11-21 05:21:08 +0200

    chore: Remove GuitarApp.

--- apps/guitar-app/src/main.ts
+++
@@ -1,9 +0,0 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./style.css";
-
-import { GuitarApp } from "./guitar-app";
-
-GuitarApp.start();
