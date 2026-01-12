8e36ee76dbdbbe7b0ee0cd92c1a9e0dd93da4daf
Stefan B. <pahkasoft@outlook.com>
2026-01-12 12:17:23 +0200

    feat: Add Core.getLibVersion() and Core.getLibInfo().

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,17 +1,26 @@
+import PackageJson from "../../package.json";
 import "./custom-css";
 
 export * from "./error";
 
+declare const __LIB_VERSION__: string;
 declare const __LIB_INFO__: string;
+
+export function getLibVersion(): string {
+    return __LIB_VERSION__;
+}
+
+export function getLibInfo(): string {
+    return __LIB_INFO__;
+}
 
 let initialized = false;
 
 export function init(): void {
-    if (initialized) {
+    if (initialized)
         return;
-    }
     
     initialized = true;
 
-    console.log("%c" + __LIB_INFO__ + " initialized.", "background: black; color: white; padding: 2px;");
+    console.log(`%c${getLibInfo()} initialized.`, `background: black; color: white; padding: 2px;`);
 }
