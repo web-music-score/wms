38126b06e8c50fb49c3351442393db9c887cf01a
Stefan B. <web-music-score@outlook.com>
2026-01-16 04:43:37 +0200

    fix: minor edit.

--- apps/ts-example/src/main.ts
+++ apps/ts-example/src/main.ts
@@ -6,13 +6,13 @@ import { createFrereJacques } from "web-music-score/pieces";
 
 let doc = createFrereJacques();
 
-new Score.WmsView().
-    setCanvas("scoreCanvas").
-    setDocument(doc).
-    draw();
+new Score.WmsView()
+    .setCanvas("scoreCanvas")
+    .setDocument(doc)
+    .draw();
 
-new Score.WmsControls().
-    setPlayButton("playButton").
-    setPauseButton("pauseButton").
-    setStopButton("stopButton").
-    setDocument(doc);
+new Score.WmsControls()
+    .setPlayButton("playButton")
+    .setPauseButton("pauseButton")
+    .setStopButton("stopButton")
+    .setDocument(doc);
