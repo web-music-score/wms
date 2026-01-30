f8a2ebe800e2482b8c9a1912b9401aec9570d7a6
Stefan B. <developer-stefan@outlook.com>
2026-01-30 05:57:26 +0200

    refactor: typo in function name.

--- src/score/engine/enum-utils.ts
+++ src/score/engine/enum-utils.ts
@@ -6,7 +6,7 @@ export function resolveEnumValue<E extends Utils.Enum.EnumObject>(input: string,
     return Utils.Enum.getEnumValues(enumObject).find(v => String(v).toLowerCase() === normalized);
 }
 
-export function resolvedRequiredEnumValue<E extends Utils.Enum.EnumObject>(input: string, enumObject: E): Utils.Enum.EnumValue<E> {
+export function resolveRequiredEnumValue<E extends Utils.Enum.EnumObject>(input: string, enumObject: E): Utils.Enum.EnumValue<E> {
     const enumValue = resolveEnumValue(input, enumObject);
     if (enumValue === undefined)
         throw new MusicError(MusicErrorType.Score, `Invalid enum value "${input}" for enum object: ${Utils.Str.stringify(enumObject)}`);
